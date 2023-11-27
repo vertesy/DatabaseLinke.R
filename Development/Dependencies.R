5d3ee1945a2b4c00608f76d6ab4490d4aee28421
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 20:45:09 +0100

    dep

--- Development/Dependencies.R
+++ Development/Dependencies.R
@@ -1,9 +1,15 @@
+Dependency file generated on Mon Nov 27 20:44:58 2023 
+
+#################################################################################################### 
+DatabaseLinke.R
+#################################################################################################### 
 $`character(0)`
-[1] "write.simple.append"
+[1] "as.tibble"           "deframe"             "FormatAsExcelLink"   "idim"                "read_clip_tbl"      
+[6] "write_clip"          "write.simple.append"
 
 $`package:base`
-[1] "c"         "is.na"     "paste"     "paste0"    "return"   
-[6] "Sys.sleep"
+ [1] "c"         "colnames"  "gsub"      "is.na"     "ncol"      "paste"     "paste0"    "print"     "return"   
+[10] "stopifnot" "Sys.sleep"
 
 $`package:DatabaseLinke.R`
 [1] "openURLs.1by1"
@@ -12,10 +18,17 @@ $`package:stats`
 [1] "runif"
 
 $`package:utils`
-[1] "browseURL"       "globalVariables"
+[1] "browseURL"       "globalVariables" "head"           
 
 character(0)
 base
 DatabaseLinke.R
 stats
 utils
+#################################################################################################### 
+DatabaseLinke.Setup.Global.Variables.R
+#################################################################################################### 
+$`package:base`
+[1] "as.vector"  "c"          "options"    "Sys.getenv" "Sys.setenv"
+
+base
